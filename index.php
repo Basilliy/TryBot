fbfe864797056244b6d6367837b060e5fb13faf4
russik2013 <z.kon2009@yandex.ru>
2016-07-13 12:39:30 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$access_token = '246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA';
+$url = 'https://api.telegram.org/bot' . $access_token;
+
+$output = json_decode(file_get_contents('php://input'), true);
+$chat_id = $output['message']['chat']['id'];
+$first_name = $output['message']['chat']['first_name'];
+$message = $output['message']['text'];
+print_r('out=',serialize($output));
+
+$fuck = file_get_contents('https://evilinsult.com/generate_insult.php?lang=en');
+//echo $update;
+$update = file_get_contents($url."/sendmessage?chat_id=".$chat_id."&text=$fuck");
