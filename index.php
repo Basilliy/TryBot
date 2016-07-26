aedaaf3c799ed33ece32e7fb73cd7cbd7a940484
Basilliy <z.kon2009@gmail.com>
2016-07-26 11:59:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,13 @@ ini_set('display_startup_errors', 1);
 $database = 'upperl_vadik'; // имя базы данных
 $user = 'upperl_vadik'; // имя пользователя
 $password = '2shmpzez'; // пароль
-$link = mysql_connect($host, $user, $password )
+$link = mysqli_connect($host, $user, $password,$database )
     or die('Не удалось соединиться: ' . mysql_error());
 echo 'Соединение успешно установлено';
-mysql_select_db($database) or die('Не удалось выбрать базу данных');
 
 // Выполняем SQL-запрос
 $query = 'SELECT * FROM menu';
-$result = mysql_query($query, $database) or die('Запрос не удался: ' . mysql_error());
+$result = mysqli_query($query) or die('Запрос не удался: ' . mysql_error());
 print_r($result);
 
 // Освобождаем память от результата
