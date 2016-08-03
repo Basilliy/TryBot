9eb55e6ef30d436a0fae3f1f05ac90625f5f7be8
Basilliy <z.kon2009@gmail.com>
2016-08-03 13:54:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -169,7 +169,7 @@ for($i = 0; $i < count($button); $i++){
 $link->close();
 
         $reply_markup = '';
-     $buttons = [[$button],[$button]];
+     $buttons = [$button[0]];
     $keyboard = json_encode($keyboard = [
         'keyboard' => $buttons /*[$buttons]*/,
         'resize_keyboard' => true,
