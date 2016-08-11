5f92bda29c321b2faf185389969f05c1f91c90d9
Basilliy <z.kon2009@gmail.com>
2016-08-11 17:37:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $chat_id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 $fp = json_decode(file_get_contents('user.json'), true);
 
-if(isset($output['inline_query']){
+if(isset($output['inline_query'])){
     file_get_contents("https://api.telegram.org/bot246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA/sendMessage?chat_id=267280685&text=hi"); 
 }
 
