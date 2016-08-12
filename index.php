f23b4cbe8976ee64c931eed913db0758157858b7
Basilliy <z.kon2009@gmail.com>
2016-08-12 14:00:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if(isset($output['inline_query'])){
                   "photo_url" => "https://martinfacebook.herokuapp.com/evil.jpg",
                   "trumb_url" => "https://martinfacebook.herokuapp.com/evil.jpg"
                   );
-    $drug = json_encode([$rus]);
+    $drug = json_encode($rus);
     file_put_contents("errors.txt",$drug);
     file_get_contents("https://api.telegram.org/bot246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA/answerInlineQuery?inline_query_id=".$output['inline_query']['id']."&results=".$drug); 
 }
